69d186a81b4e58ec7009bd62b5af38c9cc0881a2
KAIVIOR <wupeter99989@gmail.com>
2022-01-15 23:26:29 +0800

    search.html
    把美食收尋站補上去 和 隨機選擇用好

---
+++ sonar-master/js/search.js
@@ -0,0 +1,45 @@
+const charactersList = document.getElementById('charactersList');
+const searchBar = document.getElementById('searchBar');
+let hpCharacters = [];
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredCharacters = hpCharacters.filter((character) => {
+        return (
+            character.name.toLowerCase().includes(searchString) ||
+            character.house.toLowerCase().includes(searchString)
+        );
+    });
+    displayCharacters(filteredCharacters);
+});
+
+const loadCharacters = async () => {
+    try {
+        const res = await fetch('https://run.mocky.io/v3/e490ac1d-1339-40d4-a064-9d075f56356a');
+        hpCharacters = await res.json();
+        displayCharacters(hpCharacters);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p>House: ${character.house}</p>
+                <img src="${character.image}"></img>
+                <a href="${character.recipe} styles = 'color:black;'">菜單</a>
+            </li>
+        `;
+    
+        })
+        .join('');
+        
+    charactersList.innerHTML = htmlString;
+};
+
+loadCharacters();
